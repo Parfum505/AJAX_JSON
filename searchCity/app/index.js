996e64de624492f10df394679432dbce4d5a2002
Pavel <parfum505@gmail.com>
2017-02-19 14:46:07 +0100

    add js to twitchTV project

--- searchCity/app/index.js
+++ searchCity/app/index.js
@@ -2,15 +2,37 @@
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 const cities = [];
 
-let req = new XMLHttpRequest();
+const req = new XMLHttpRequest();
 	req.onreadystatechange = function() {
 	    if (this.readyState == 4 && this.status == 200) {
 			let answ = JSON.parse(this.responseText);
 			cities.push(...answ);
-			console.log(cities[0]);
+			// console.log(cities);
 	    }
 	};
 	req.open("GET", url, true);
 	req.send();
 
-console.log(cities[0]);
+	function buildList(list, text, reg) {
+
+		const html = list.map(item => {
+			const city = item.city.replace(reg, `<span class="hl">${text}</span>`);
+			const state = item.state.replace(reg, `<span class="hl">${text}</span>`);
+			let li = '<li><span class="name">';
+			li += city + ', '+ state +'</span>';
+			li +='<span class="population">' + item.population+'</span></li>';
+			return li;
+		}).join("");
+
+		// console.log(html);
+		document.querySelector('.suggestions').innerHTML = html;
+	}
+	function searchCity() {
+		const text = this.value;
+		const reg = new RegExp(text, 'gi');
+		const list = cities.filter(city => city.city.search(reg) != -1 || city.state.search(reg) != -1);
+		buildList(list, text, reg);
+		// console.log(list);
+	}
+
+document.querySelector('.search').addEventListener('keyup', searchCity, false);
--- searchCity/build/script.js
+++ searchCity/build/script.js
@@ -73,8 +73,47 @@
 "use strict";
 
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-console.log(url, 'all right');
+var cities = [];
+
+var req = new XMLHttpRequest();
+req.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		var answ = JSON.parse(this.responseText);
+		cities.push.apply(cities, _toConsumableArray(answ));
+		// console.log(cities);
+	}
+};
+req.open("GET", url, true);
+req.send();
+
+function buildList(list, text, reg) {
+
+	var html = list.map(function (item) {
+		var city = item.city.replace(reg, '<span class="hl">' + text + '</span>');
+		var state = item.state.replace(reg, '<span class="hl">' + text + '</span>');
+		var li = '<li><span class="name">';
+		li += city + ', ' + state + '</span>';
+		li += '<span class="population">' + item.population + '</span></li>';
+		return li;
+	}).join("");
+
+	// console.log(html);
+	document.querySelector('.suggestions').innerHTML = html;
+}
+function searchCity() {
+	var text = this.value;
+	var reg = new RegExp(text, 'gi');
+	var list = cities.filter(function (city) {
+		return city.city.search(reg) != -1 || city.state.search(reg) != -1;
+	});
+	buildList(list, text, reg);
+	// console.log(list);
+}
+
+document.querySelector('.search').addEventListener('keyup', searchCity, false);
 
 /***/ }),
 /* 1 */
