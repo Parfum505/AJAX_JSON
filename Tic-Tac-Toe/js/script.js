55520c279d4ec05edc1e1ca3fdf8d003f583c157
Pavel <parfum505@gmail.com>
2017-11-11 14:19:30 +0100

    add startGame and turnClick func to script.js

--- Tic-Tac-Toe/js/script.js
+++ Tic-Tac-Toe/js/script.js
@@ -1,3 +1,32 @@
 var board;
-const player = 'O';
-const AI = 'X';
+const huPlayer = 'O';
+const aiPlayer = 'X';
+const winCombos = [
+	[0,1,2],
+	[3,4,5],
+	[6,7,8],
+	[0,4,8],
+	[6,4,2],
+	[0,3,6],
+	[1,4,7],
+	[2,5,8]
+];
+const cells = document.querySelectorAll('.cell');
+
+function startGame(){
+	document.querySelector('.endGame').style.display = 'none';
+	board = Array.from(Array(9).keys());
+	for(var i=0; i < cells.length; i++){
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+function turnClick(){
+	turn(this.id, huPlayer);
+}
+function turn(id, player){
+	board[id] = player;
+	document.getElementById(id).innerText = player;
+	//console.log(board);
+}
