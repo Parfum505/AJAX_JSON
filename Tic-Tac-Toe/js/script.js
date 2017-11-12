7cc1a907421a699146694a53943fd6816db0e2ba
Pavel <parfum505@gmail.com>
2017-11-12 15:46:29 +0100

    add checkWin and gameOver func to script.js

--- Tic-Tac-Toe/js/script.js
+++ Tic-Tac-Toe/js/script.js
@@ -1,4 +1,4 @@
-var board;
+var origBoard;
 const huPlayer = 'O';
 const aiPlayer = 'X';
 const winCombos = [
@@ -15,10 +15,10 @@ const cells = document.querySelectorAll('.cell');
 
 function startGame(){
 	document.querySelector('.endGame').style.display = 'none';
-	board = Array.from(Array(9).keys());
+	origBoard = Array.from(Array(9).keys());
 	for(var i=0; i < cells.length; i++){
 		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
+		cells[i].style.removeProperty('color');
 		cells[i].addEventListener('click', turnClick, false);
 	}
 }
@@ -26,7 +26,36 @@ function turnClick(){
 	turn(this.id, huPlayer);
 }
 function turn(id, player){
-	board[id] = player;
+	origBoard[id] = player;
 	document.getElementById(id).innerText = player;
-	//console.log(board);
+	let gameWon = checkWin(origBoard, player);
+	if (gameWon) gameOver(gameWon);
+}
+function checkWin(board, player) {
+	let plays = [];
+	for(var i =0 ; i < board.length; i++) {
+		if(board[i] == player) plays.push(i);
+	}
+	let gameWon = null;
+	for(var i=0; i<winCombos.length; i++){
+		if ((plays.indexOf(winCombos[i][0]) > -1) &&
+			(plays.indexOf(winCombos[i][1]) > -1) &&
+			(plays.indexOf(winCombos[i][2]) > -1)) {
+			gameWon = {index: i , player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	var winArr = winCombos[gameWon.index];
+	for (var i=0; i<3; i++){
+		document.getElementById(winArr[i]).style.color =
+			gameWon.player == huPlayer ? "blue" : "red";
+	}
+
 }
